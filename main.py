d43703e85f31efa437796d42632a50ef17d2198f
Raul Sood <raulsood@Rauls-MacBook-Pro.local>
2022-07-12 11:25:44 +0200

    Basic setup created, debugging enabled, testing enabled

---
+++ main.py
@@ -0,0 +1,23 @@
+def coding_challenge_1() -> int:
+    print("coding challenge 1")
+    return 1
+
+
+def coding_challenge_2() -> int:
+    print("coding challenge 2")
+    return 2
+
+
+def coding_challenge_3() -> int:
+    print("coding challenge 3")
+    return 3
+
+
+def coding_challenge_4() -> int:
+    print("coding challenge 4")
+    return 4
+
+
+def coding_challenge_5() -> int:
+    print("coding challenge 5")
+    return 5
---
+++ test_main.py
@@ -0,0 +1,21 @@
+from main import *
+
+
+def test_coding_challenge_1() -> int:
+    assert 1 == coding_challenge_1()
+
+
+def test_coding_challenge_2() -> int:
+    assert 1 == coding_challenge_1()
+
+
+def test_coding_challenge_3() -> int:
+    assert 1 == coding_challenge_1()
+
+
+def test_coding_challenge_4() -> int:
+    assert 1 == coding_challenge_1()
+
+
+def test_coding_challenge_5() -> int:
+    assert 1 == coding_challenge_1()
